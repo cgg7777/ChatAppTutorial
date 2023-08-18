8126ca553d475ae9ae30ca6a86eac943ff1e9146
Geomgi <cgg7777@naver.com>
2023-08-18 16:30:21 +0900

    Chatroom 생성 db 반영

--- src/components/ChatPage/SidePanel/ChatRoom.js
+++ src/components/ChatPage/SidePanel/ChatRoom.js
@@ -3,13 +3,53 @@ import { FaRegSmileWink } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-
+import { connect } from "react-redux";
+import { getDatabase, push, ref, update } from "firebase/database";
 export class ChatRoom extends Component {
     state = {
         show: false,
+        name: "",
+        description: "",
+        chatRoomsRef: ref(getDatabase(), "chatRooms"),
     };
     handleClose = () => this.setState({ show: false });
     handleShow = () => this.setState({ show: true });
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, description } = this.state;
+        if (this.isFormValid(name, description)) {
+            this.addChatRoom();
+        }
+    };
+    addChatRoom = async () => {
+        const key = push(this.state.chatRoomsRef).key;
+        const { name, description } = this.state;
+        const { user } = this.props;
+        console.log(user);
+        const newChatRoom = {
+            id: key,
+            name: name,
+            description: description,
+            createdBy: {
+                name: user.displayName,
+                image: user.photoURL,
+            },
+        };
+        try {
+            const db = getDatabase();
+            const updates = {};
+            updates[key] = newChatRoom;
+            await update(this.state.chatRoomsRef, updates);
+            this.setState({
+                name: "",
+                description: "",
+                show: false,
+            });
+        } catch (error) {
+            alert(error);
+        }
+    };
+    isFormValid = (name, description) => name && description;
     render() {
         return (
             <div>
@@ -25,15 +65,15 @@ export class ChatRoom extends Component {
                             <Modal.Title>Create a chat room</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <Form>
+                            <Form onSubmit={this.handleSubmit}>
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Label>방 이름</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter a chat room name" />
+                                    <Form.Control onChange={(e) => this.setState({ name: e.target.value })} type="text" placeholder="Enter a chat room name" />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>방 설명</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter a chat room description" />
+                                    <Form.Control onChange={(e) => this.setState({ description: e.target.value })} type="text" placeholder="Enter a chat room description" />
                                 </Form.Group>
                             </Form>
                         </Modal.Body>
@@ -41,8 +81,8 @@ export class ChatRoom extends Component {
                             <Button variant="secondary" onClick={this.handleClose}>
                                 Close
                             </Button>
-                            <Button variant="primary" onClick={this.handleClose}>
-                                Save Changes
+                            <Button variant="primary" onClick={this.handleSubmit}>
+                                방 생성
                             </Button>
                         </Modal.Footer>
                     </Modal>
@@ -51,5 +91,9 @@ export class ChatRoom extends Component {
         );
     }
 }
-
-export default ChatRoom;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user.currentUser,
+    };
+};
+export default connect(mapStateToProps)(ChatRoom);
