71b54b23ef90339801e035f13d6e79e8dcad6d1f
cgg7777 <cgg7777@naver.com>
2023-08-19 14:05:23 +0900

    채팅방 이동 기능 구현

--- src/components/ChatPage/SidePanel/ChatRoom.js
+++ src/components/ChatPage/SidePanel/ChatRoom.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { DataSnapshot, getDatabase, push, ref, update } from "firebase/database";
+import { setCurrentChatRoom } from "../../../redux/actions/chatRoom_action";
 import { onChildAdded } from "firebase/database";
 export class ChatRoom extends Component {
     state = {
@@ -13,15 +14,25 @@ export class ChatRoom extends Component {
         description: "",
         chatRoomsRef: ref(getDatabase(), "chatRooms"),
         chatRooms: [],
+        firstLoad: true,
+        activeChatRoomID: "",
     };
     componentDidMount() {
         this.AddChatRoomsListensers();
+    }
+    setFirstChatRoom() {
+        const firstChatRoom = this.state.chatRooms[0];
+        if (this.state.firstLoad && this.state.chatRooms.length > 0) {
+            this.props.dispatch(setCurrentChatRoom(firstChatRoom));
+            this.setState({ activeChatRoomID: firstChatRoom.id });
+        }
+        this.setState({ firstLoad: false });
     }
     AddChatRoomsListensers = () => {
         let chatRoomsArray = [];
         onChildAdded(this.state.chatRoomsRef, (DataSnapshot) => {
             chatRoomsArray.push(DataSnapshot.val());
-            this.setState({ chatRooms: chatRoomsArray });
+            this.setState({ chatRooms: chatRoomsArray }, () => this.setFirstChatRoom());
         });
     };
     handleClose = () => this.setState({ show: false });
@@ -62,8 +73,21 @@ export class ChatRoom extends Component {
         }
     };
     isFormValid = (name, description) => name && description;
+    changeChatRoom = (room) => {
+        this.props.dispatch(setCurrentChatRoom(room));
+        this.setState({ activeChatRoomID: room.id });
+    };
     renderChatRooms = (chatRooms) =>
-        chatRooms.length > 0 && chatRooms.map((room) => <li key={room.id}>#{room.name}</li>);
+        chatRooms.length > 0 &&
+        chatRooms.map((room) => (
+            <li
+                style={{ backgroundColor: room.id == this.state.activeChatRoomID && "#ffffff45" }}
+                onClick={() => this.changeChatRoom(room)}
+                key={room.id}
+            >
+                #{room.name}
+            </li>
+        ));
 
     render() {
         return (
